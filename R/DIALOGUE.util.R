b0b7172e3ad644e3e92317edcb01825680bf11b3
Edoardo Giacopuzzi <51458073+edg1983@users.noreply.github.com>
2024-09-25 15:30:43 +0200

    improve matrix checks in average.mat.rows

--- R/DIALOGUE.util.R
+++ R/DIALOGUE.util.R
@@ -16,7 +16,7 @@ average.mat.rows<-function(m,ids,f = colMeans){
     message("WARN - m0 has zero columns")
     return(m1)
   }
-  if(sum(b)==1){m0<-t(as.matrix(m0))}
+  if(sum(b)==1 & dim(m0)[2] != dim(m1)[2]){m0<-t(as.matrix(m0))}
   rownames(m0)<-ids[b]
 
   message("Making m2")
