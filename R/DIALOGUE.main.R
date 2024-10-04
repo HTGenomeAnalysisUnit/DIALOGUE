64432dffc6403e3ece813566502501fb8529f6ed
Edoardo Giacopuzzi <51458073+edg1983@users.noreply.github.com>
2024-10-04 12:16:42 +0200

    fixed missing comma message

--- R/DIALOGUE.main.R
+++ R/DIALOGUE.main.R
@@ -183,7 +183,7 @@ DIALOGUE1<-function(rA,main,param){
   samples_per_celltype <- lapply(X, rownames)
   message("=== SAMPLES SEEN IN EACH CELLTYPE ===")
   for (ct in names(samples_per_celltype)) {
-    message(">>> " ct " - ", length(samples_per_celltype[[ct]]), " <<<")
+    message(">>> ", CT, " - ", length(samples_per_celltype[[ct]]), " <<<")
     message(paste(sort(samples_per_celltype[[ct]]), collapse=",")
   }
   message("=====================================")
